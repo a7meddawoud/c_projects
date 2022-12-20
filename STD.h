fbb44da8e619fff00de21606f38f743c866f5951
Ahmed Dawoud <a7med.dawoud@gmail.com>
2022-12-20 02:28:26 +0200

    redefine

---
+++ STD.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//----------- redefine commom used data types to be used easly ------------
+typedef unsigned char uint8;
+typedef char Int8;
+typedef unsigned short uint16;
+typedef short int16;
+typedef unsigned int uint32;
+typedef int int32;
+typedef enum {false, true} bool;
